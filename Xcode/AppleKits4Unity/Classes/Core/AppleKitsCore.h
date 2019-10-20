6c0b4e67c03e54eab9f7781fe750df004cc788be
Alex Griffin <alexgriffn@gmail.com>
2019-10-20 22:42:04 +1100

    test push

---
+++ Xcode/AppleKits4Unity/Classes/Core/AppleKitsCore.h
@@ -0,0 +1,17 @@
+//
+//  AppleKitsCore.h
+//  AppleKits4Unity
+//
+//  Created by Alex Griffin on 20/10/19.
+//  Copyright © 2019 Forbidden Cactus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppleKitsCore : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Xcode/AppleKits4Unity/Classes/Core/AppleKitsCore.m
@@ -0,0 +1,13 @@
+//
+//  AppleKitsCore.m
+//  AppleKits4Unity
+//
+//  Created by Alex Griffin on 20/10/19.
+//  Copyright © 2019 Forbidden Cactus. All rights reserved.
+//
+
+#import "AppleKitsCore.h"
+
+@implementation AppleKitsCore
+
+@end
